7c1da5c4c05e5e5f6c51b5b806212f94a1f88a39
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-05-12 09:15:05 -0300

    feat: add button variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import { Button } from '@/components/generic/Button/Button'
 import { Text } from '@/components/generic/Text/Text'
 
 export default function Home() {
   return (
     <div>
       <Text>Home</Text>
+      <Button>Placeholder</Button>
+      <Button variant="secondary">Placeholder</Button>
+      <Button variant="tertiary" size="tertiary">
+        Placeholder
+      </Button>
+      <Button variant="destroy">Placeholder</Button>
     </div>
   )
 }
