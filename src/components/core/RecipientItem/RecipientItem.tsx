034665e061e584daa81bbfd127278b7507c18d18
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-05-14 22:29:40 -0300

    refactor: update TransactionItem to RecipientItem

--- src/components/core/RecipientItem/RecipientItem.tsx
+++ src/components/core/RecipientItem/RecipientItem.tsx
@@ -1,23 +1,17 @@
 import { Text } from '@/components/generic/Text/Text'
-import { Transaction } from '../Transactions'
-import Image from 'next/image'
+import { Transaction } from '@/models/transaction/transaction'
+import { Recipient, RecipientProps } from '../Recipient/Recipient'
 
-export function TransactionItem(props: Transaction) {
+export function RecipientItem(props: Transaction & RecipientProps) {
   return (
     <div className="h-[68px] not-last:border-b not-last:border-b-grey-100 flex items-center justify-between">
-      <div className="flex items-center gap-[16px]">
-        <Image
-          src={props.avatar}
-          alt={props.name}
-          width={32}
-          height={32}
-          className="w-[32px] h-[32px] bg-green rounded-full"
-        />
-        <Text className="text-grey-900" variant="preset-4-bold">
-          {props.name}
-        </Text>
-      </div>
-      <div>
+      <Recipient
+        showCategory={props.showCategory}
+        avatar={props.avatar}
+        category={props.category}
+        name={props.name}
+      />
+      <div className="text-right">
         <Text variant="preset-4-bold" className="text-green">
           +{props.amount}
         </Text>
