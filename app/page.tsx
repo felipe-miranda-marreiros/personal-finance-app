f32659ebe65b6c89c9c919358e537b8db71837d2
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-08 00:10:20 -0300

    feat: add default redirect to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-import { Button } from '@/shared/components/generic/Button/Button'
-import { Input } from '@/shared/components/generic/Input/Input'
-import { Text } from '@/shared/components/generic/Text/Text'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <div>
-      <Text>Home</Text>
-      <Button>Placeholder</Button>
-      <Button variant="secondary">Placeholder</Button>
-      <Button variant="tertiary" size="tertiary">
-        Placeholder
-      </Button>
-      <Button variant="destroy">Placeholder</Button>
-      <Input rightIcon="AArrowDown" leftIcon="AArrowDown" placeholder="Placeholder" />
-    </div>
-  )
+  redirect('/dashboard/overview')
 }
